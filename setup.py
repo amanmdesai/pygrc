a3ca83bd80e8c1cd57477373a5e79f48369aaedd
amanmdesai <amanmukeshdesai@gmail.com>
2023-02-07 11:50:27 +0530

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md','r') as file:
 
 setuptools.setup(
 name="pygrc",
-version="0.1.0",
+version="0.2.0",
 description= "A package to read SPARC data for Galactic Rotation Curves",
 author="Aman Desai",
 author_email="amanmukeshdesai@gmail.com",
